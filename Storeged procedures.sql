6d96a52857c21a137508669b6394abde460461d5
adjany08 <84366189+adjany08@users.noreply.github.com>
2022-10-30 02:24:53 -0600

    Procedimientos para el mantenimientos de tareas

--- Storeged procedures.sql
+++ Storeged procedures.sql
@@ -12,25 +12,16 @@ create procedure agregarFamilia
 @nombre varchar (20),
 @descripcion varchar (50)
 as
-IF EXISTS (select codigo from familia_producto where codigo = @codigo)
-	BEGIN
-		return -1;
-	END
-ELSE
-BEGIN TRY
-	insert into familia_producto(codigo, nombre, descripcion) 
-	values(@codigo, @nombre, @descripcion)
-	return 0
-END TRY
-BEGIN CATCH
-	return -1
-end catch
+begin
+insert into familia_producto(codigo, nombre, descripcion) 
+values
+(@codigo, @nombre, @descripcion)
+end
 
 --Procedimientos almacenados para manejar los productos
 
 
 go
-
 create procedure agregarProducto
 	@codigo varchar(10), 
 	@nombre varchar(10), 
@@ -39,24 +30,16 @@ create procedure agregarProducto
 	@activo smallint, 
 	@codigo_familia varchar(10)
 as
-IF EXISTS (select codigo from producto where codigo = @codigo)
-	BEGIN
-		return -1;
-	END
-ELSE
-BEGIN
-	BEGIN TRY
-		INSERT INTO producto(codigo, nombre, descripcion,precio,activo,codigo_familia ) 
-		VALUES(@codigo, @nombre, @descripcion,@precio,@activo,@codigo_familia)
-		return 0
-	END TRY
-	BEGIN CATCH
-		return -1
-	END CATCH;
-END
+begin
+insert into producto(codigo, nombre, descripcion,precio,activo,codigo_familia ) 
+values
+(@codigo, @nombre, @descripcion,@precio,@activo,@codigo_familia)
+end
+
+
+
 
 go
-
 create procedure editarProducto
 	@codigo varchar(10), 
 	@nombre varchar(10), 
@@ -66,15 +49,9 @@ create procedure editarProducto
 	@familiaProducto varchar(10)
 as
 begin
-BEGIN TRY
-	UPDATE producto
-	SET codigo = @codigo, nombre = @nombre ,descripcion = @descripcion, precio = @precio, activo = @activo, codigo_familia = @familiaProducto
+UPDATE producto
+SET codigo = @codigo, nombre = @nombre ,descripcion = @descripcion, precio = @precio, activo = @activo, codigo_familia = @familiaProducto
 	Where @codigo = codigo;
-	return 0
-END TRY
-BEGIN CATCH
-	return -1
-END CATCH;
 end
 
 
@@ -84,7 +61,6 @@ end
 --Procedimientos almacenados para el manejo de usuarios
 
 go
-
 create procedure agregarUsuario
 @cedula varchar(10),
 @nombre varchar (20),
@@ -96,43 +72,25 @@ create procedure agregarUsuario
 @departamento smallint,
 @patron varchar(20)
 as
-IF EXISTS (select cedula from usuario where cedula = @cedula)
-	BEGIN
-		return -1;
-	END
-ELSE
 begin
-BEGIN TRY
-	insert into usuario(cedula, nombre, apellido1,apellido2, nombre_usuario, clave, departamento, rol) 
-	values(@cedula, @nombre, @apellido1, @apellido2, @nombre_usuario, ENCRYPTBYPASSPHRASE (@patron, @clave), @departamento, @rol)
-	RETURN 0
-END TRY
-BEGIN CATCH 
-	RETURN -1
-END CATCH;
+insert into usuario(cedula, nombre, apellido1,apellido2, nombre_usuario, clave, departamento, rol) 
+values
+(@cedula, @nombre, @apellido1, @apellido2, @nombre_usuario, ENCRYPTBYPASSPHRASE (@patron, @clave), @departamento, @rol)
 end
 
-
-
 go
-
 create procedure validarUsuario
 @usario varchar(20),
 @clave varchar(30),
 @patron varchar(20)
 as
-BEGIN TRY
-	select * from usuario where nombre_usuario = @usario and CONVERT(varchar(30), DECRYPTBYPASSPHRASE(@patron, clave))= @clave
-	return 0
-END TRY
-BEGIN CATCH
-	return -1
-END CATCH;
+begin
+select * from usuario where nombre_usuario = @usario and CONVERT(varchar(30), DECRYPTBYPASSPHRASE(@patron, clave))= @clave
+end
 
 --Procedimientos almacenados para agregar clientes
 
 go
-
 create procedure agregarCliente
 @nombre_cuenta varchar(10),
 @celular varchar (20),
@@ -145,19 +103,11 @@ create procedure agregarCliente
 @sector smallint,
 @moneda smallint
 as
-IF EXISTS (select nombre_cuenta from cliente where nombre_cuenta = @nombre_cuenta)
-	BEGIN
-		return -1;
-	END
-ELSE
-begin try
-	insert into cliente(nombre_cuenta, celular, telefono,correo, sitio, contacto_principal, asesor, IDzona,IDsector, IDmoneda) 
-	values(@nombre_cuenta, @celular, @telefono,@correo, @sitio, @contactoP, @asesor, @zona,@sector,@moneda ) 
-	return 0 
-end try
-begin catch
-	return -1
-end catch
+begin
+insert into cliente(nombre_cuenta, celular, telefono,correo, sitio, contacto_principal, asesor, IDzona,IDsector, IDmoneda) 
+values
+(@nombre_cuenta, @celular, @telefono,@correo, @sitio, @contactoP, @asesor, @zona,@sector,@moneda ) 
+end
 
 
 
@@ -165,7 +115,6 @@ end catch
 
 
 go
-
 create procedure agregarContacto
 @idContacto smallint,
 @nombre varchar (20),
@@ -182,69 +131,43 @@ create procedure agregarContacto
 @estado smallint
 
 as
-IF EXISTS (select nombre from contacto where nombre = @nombre)
-	BEGIN
-		return -1;
-	END
-ELSE
-begin try
+begin
 insert into contacto
-	values	(@idContacto, @nombre, @motivo, @telefono, @correo, @direccion, @descripcion,@cliente, @zona,@sector, @asesor, @tipoContacto, @estado)
-	return 0
-end try
-begin catch 
-	return -1
-end catch
+values
+(@idContacto, @nombre, @motivo, @telefono, @correo, @direccion, @descripcion,@cliente, @zona,@sector, @asesor, @tipoContacto, @estado)
+end
 
 
 --Storaged procedures para catalogos 
 
 go
-
 create procedure agregarTipoContacto
 @id smallint,
 @tipo varchar (20)
 as
-IF EXISTS (select id from tipoContacto where id = @id)
-	BEGIN
-		return -1;
-	END
-ELSE
-begin try
-	insert into tipoContacto
-	values(@id, @tipo)
-	return 0
-end try
-begin catch
-	return -1
-end catch;
+begin
+insert into tipoContacto
+values
+(@id, @tipo)
+end
+
+
 
 
 
 go
-
 create procedure agregarEstado
 @id smallint,
 @estado varchar (20)
 as
-IF EXISTS (select id from estado where id = @id)
-	BEGIN
-		return -1;
-	END
-ELSE
-begin try
-	insert into estado
-	values(@id, @estado)
-	return 0
-end try
-begin catch
-	return -1
-end catch
-
+begin
+insert into estado
+values
+(@id, @estado)
+end
 
 
 go
-
 create procedure agregarTarea
 
 @idContacto smallint,
@@ -255,65 +178,45 @@ create procedure agregarTarea
 @fechaCreacion date,
 @asesor varchar(10)
 as
-IF EXISTS (select id from tarea where id = @idContacto)
-	BEGIN
-		return -1;
-	END
-ELSE
-begin try
+begin
 insert into tarea
-	values (@id, @fechaFinalizacion, @fechaCreacion, @informacion,@asesor, @estado)
-	INSERT INTO tareaXcontacto VALUES (@idContacto, @id)
-	return 0
-end try
-begin catch
-	return -1
-end catch
+values 
+(@id, @fechaFinalizacion, @fechaCreacion, @informacion,@asesor, @estado)
+INSERT INTO tareaXcontacto VALUES (@idContacto, @id)
+end
 
 
 
 go
-
 create procedure agregarActividad
+
+@idContacto smallint,
 @id smallint,
 @descripcion varchar (25),
 @fechaIni date,
 @fechaFin date,
 @asesor varchar(10)
 as
-IF EXISTS (select id from actividad where id = @id)
-	BEGIN
-		return -1;
-	END
-ELSE
-begin try
-	insert into actividad
-	values (@id, @descripcion, @fechaIni, @fechaFin,@asesor)
-	return 0
-end try
-begin catch
-	return -1
-end catch;
-
+begin
+insert into actividad
+values
+(@id, @descripcion, @fechaIni, @fechaFin,@asesor)
+INSERT INTO actividadesXcontacto VALUES (@idContacto, @id)
+end
 
 go
-
 create procedure agregarCxA
 @contacto smallint,
 @actividad smallint
 as
-begin try
-	insert into actividadesXcontacto
-	values(@contacto, @actividad)
-	return 0;
-end try
-begin catch
-	return -1
-end catch;
+begin
+insert into actividadesXcontacto
+values
+(@contacto, @actividad)
+end
 
 
 go
-
 create procedure agregarCxT
 @contacto smallint,
 @tarea smallint
@@ -329,7 +232,6 @@ end
 --Stored Procedures para cotizaciones
 
 go
-
 create procedure agregarCotizacion
 @numeroCot varchar(10),
 @nombreOpor varchar (10),
@@ -352,36 +254,25 @@ create procedure agregarCotizacion
 @contraQuien varchar(20)
 
 as
-begin try
-	insert into cotizaciones
-	values
-	(@numeroCot, @nombreOpor, @fechaCot, @fechaCierre, @ordenCompra, @descripcion, @factura, @zona, @sector, 
-	@moneda, @contactoAsociado, @asesor,  @nombreCuenta, @etapa, @probabilidad, @tipo, @razon, @contraQuien)
-	return 0
-end try
-begin catch
-	return -1;
-end catch;
+begin
+insert into cotizaciones
+values
+(@numeroCot, @nombreOpor, @fechaCot, @fechaCierre, @ordenCompra, @descripcion, @factura, @zona, @sector, 
+@moneda, @contactoAsociado, @asesor,  @nombreCuenta, @etapa, @probabilidad, @tipo, @razon, @contraQuien)
+end
 
 
 go
-
 create procedure validarContacto
 @contacto smallint
-
 as
-begin try
-	select * from contacto where idContacto = @contacto
-	return 0;
-end try
-begin catch
-	return -1
-end catch;
+begin
+select * from contacto where idContacto = @contacto
+end
 
 
 
 go
-
 create procedure editarCotizacion
     @numeroCot varchar(10),
 	@nombreOportunidad varchar(10), 
@@ -399,35 +290,57 @@ create procedure editarCotizacion
 
 
 as
-begin try
-	UPDATE cotizaciones
-	SET nombreOportunidad = @nombreOportunidad, fechaCotizacion = @fechaCotizacion, fechaCierra = @fechaCierre,
-		ordenCompra = @ordenCompra, factur = @factura, moneda = @moneda, etapa = @etapa, probabilidad = @probabilidad,
-		tipo = @tipo, razonDenegacion = @razon, contraQuien = @contraQuien, descripcion = @descripcion
+begin
+UPDATE cotizaciones
+SET nombreOportunidad = @nombreOportunidad, fechaCotizacion = @fechaCotizacion, fechaCierra = @fechaCierre,
+	ordenCompra = @ordenCompra, factur = @factura, moneda = @moneda, etapa = @etapa, probabilidad = @probabilidad,
+	tipo = @tipo, razonDenegacion = @razon, contraQuien = @contraQuien, descripcion = @descripcion
 
 	Where @numeroCot = numeroCotizacion;
-	return 0;
-end try
-begin catch
-	return -1;
-end catch
+end
 
 
--- select * from contacto
 
-go
+select * from contacto
+
+
 
 create procedure agregarProductos
 @codigo varchar(10),
 @numeroCot varchar(10),
 @cantidad smallint,
 @precioNegociado decimal(9,2)
-
 as
-begin try
-	insert into productosXcotizacion
-	values(@codigo, @numeroCot, @cantidad, @precioNegociado)
-end try
-begin catch
-	return -1
-end catch;
+begin
+
+insert into productosXcotizacion
+values(@codigo, @numeroCot, @cantidad, @precioNegociado)
+end
+
+
+
+go
+create procedure editarTarea
+	@id  smallint, 
+	@fechafin date, 
+	@asesor varchar(10),
+	@estado smallint
+as
+begin
+UPDATE tarea
+SET fechaFinalizacion =  @fechafin, asesor = @asesor, estado = @estado
+	Where @id = id;
+end
+
+
+go
+create procedure editarActividad
+	@id  smallint, 
+	@fechafin date, 
+	@asesor varchar(10)
+as
+begin
+UPDATE actividad
+SET fechaFin =  @fechafin, asesor = @asesor
+	Where @id = id;
+end
